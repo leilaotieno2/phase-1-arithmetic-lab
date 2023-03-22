16d6f83963d933dc74b65d8f3faf3fe5a480ed25
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-03-22 20:01:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply); // Output: 62
+
+let random = Math.ceil(Math.random() * 10);
+console.log(random); // Output: a random integer between 1 and 100
+let num3 = 18;
+let num4 = 7;
+
+let mod = num3 % num4;
+console.log(mod); // Output: 4
+let setOfNumbers = [5, 10, 15, 20, 17, 12];
+
+let max = Math.max(...setOfNumbers);
+console.log(max); // Output: 20
+
